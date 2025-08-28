20723a5234d4e7c368d6284a524ea9416fd931ad
Ayan Chakraborty <chakrabortyayan91235@gmail.com>
2025-08-28 13:11:14 +0530

    Port Change

--- Flash_api/app.py
+++ Flash_api/app.py
@@ -59,4 +59,4 @@ def session_audit():
 
 if __name__ == '__main__':
     # Accessible externally
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=9001, debug=True)
