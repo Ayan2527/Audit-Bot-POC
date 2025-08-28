c678140201a06857b57ad04691fcf1522ea45fea
Ayan Chakraborty <chakrabortyayan91235@gmail.com>
2025-08-28 13:15:28 +0530

    DB connection Change

--- audit_scripts/db_connection.py
+++ audit_scripts/db_connection.py
@@ -3,7 +3,7 @@ import oracledb
 
 def get_connection():
     return oracledb.connect(
-        user="audit_bot",
-        password="auditbot123",
-        dsn="127.0.0.1:1521/orclpdb1"
+        user="SYS",
+        password="Oracle123",
+        dsn="140.245.5.226:1521/ORCLPDB1"
     )
